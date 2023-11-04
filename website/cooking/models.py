9d7c1b2baf588998d4710793c783eab0d5680b61
rbieg <rbieg@uccs.edu>
2023-11-04 16:01:10 -0600

    Added appliance and ingredients model

--- website/cooking/models.py
+++ website/cooking/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Appliance(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, blank=True)
+    heat_setting_choices = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', "High"),
+        # Add more settings if needed
+        ]
+
+    heat_setting = models.CharField(max_length=20, choices=heat_setting_choices)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("appliance-detail", args=[str(self.id)])
+    
+class Ingredient(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=30, blank=True)
+    
+    cooking_method_choices = [
+        ('baking', 'Baking'),
+        ('grilling', 'Grilling'),
+        ('sauteing', 'Sautéing'),
+        ('boiling', 'Boiling'),
+        ('microwaving', 'Microwaving'),
+        # Add more cooking methods as needed
+    ]
+
+    cooking_method = models.CharField(max_length=20, choices=cooking_method_choices)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("appliance-detail", args=[str(self.id)])
+
+# class UnloggedUser(models.Model):
+#    TBI
+    
+
+# class LoggedUser(models.Model):
+#     username = models.CharField(max_length=30)
+#     email    = models.CharField(max_length=200) 
+#     password = models.CharField(max_length=20)
+
+# class Recipe(models.Model):
+#    TBI
