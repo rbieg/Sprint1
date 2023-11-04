5823f6a2eb79d62e0f52b3c44ee03a5cddc48e14
rbieg <rbieg@uccs.edu>
2023-11-04 15:13:45 -0600

    added startapp cooking

--- website/django_project/settings.py
+++ website/django_project/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cooking'
 ]
+
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
